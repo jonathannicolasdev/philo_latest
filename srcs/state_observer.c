fd381b83712106f0f08aea0144d223b2ed71bbb0
Jonathan Nicolas <jnicolas@c3r9p6.42quebec.com>
2023-03-22 16:30:01 -0400

    Add state_obsever

---
+++ srcs/state_observer.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   state_observer.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jnicolas <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 16:02:56 by jnicolas          #+#    #+#             */
+/*   Updated: 2023/03/22 16:28:43 by jnicolas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	unlock_all_forks(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	while (i < table->nb_philo)
+	{
+		pthread_mutex_unlock(&(table->fork_mutex[i]));
+		i++;
+	}
+}
+
+void	start_state_observer(t_table *table)
+{
+	int			i;
+	long long	eat_expire_time;
+
+	sleep(3);
+	while(table->dinner_inprogress)
+	{
+		while (i < table->nb_philo)
+		{
+			if (table->philo[i].eat_clock = -1)
+				break ;
+			eat_expire_time = get_time_in_ms() - table->philos[i].eat_clock;
+			if (eat_expire_time > table->todie_time)
+			{
+				log_status(&(table->philos[i], " died"));
+				table->dinner_inprogress = 0;
+				exit(0);
+			}
+			i++;
+		}
+		usleep(10);
+	}
+
+}
