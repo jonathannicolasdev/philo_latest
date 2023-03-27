0c9713646b6c17c873e3d5dc8a837d76f7620ea8
Jonathan Nicolas <jnicolas@c3r8p2.42quebec.com>
2023-03-27 16:18:04 -0400

    Add a fix for starvation of philosophers

--- srcs/dinner.c
+++ srcs/dinner.c
@@ -6,7 +6,7 @@
 /*   By: jnicolas <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 14:36:36 by jnicolas          #+#    #+#             */
-/*   Updated: 2023/03/22 16:02:00 by jnicolas         ###   ########.fr       */
+/*   Updated: 2023/03/27 16:16:01 by jnicolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,11 @@ void	thinking(t_philo *philo)
 	log_status(philo, "is thinking");
 }
 
-void	eating(t_philo *philo)
+int	forks_taken(t_philo *philo, t_table *table)
 {
-	int		nb_philo;
-	int		num;
-	int		forks_assigned;
-	t_table	*table;
-	int		left2;
-	int		right2;
+	int	is_forks_assigned;
 
-	nb_philo = philo->table->nb_philo;
-	num = philo->num;
-	table = philo->table;
-	forks_assigned = 0;
+	is_forks_assigned = 0;
 	pthread_mutex_lock(philo->table->global_mutex);
 	if (table->fork_status[philo->left_fork] == FREE
 		&& table->fork_status[philo->right_fork] == FREE)
@@ -50,59 +42,81 @@ void	eating(t_philo *philo)
 		log_status(philo, "Has taken a fork");
 	}
 	else
-		forks_assigned = 1;
+		is_forks_assigned = 1;
 	pthread_mutex_unlock(table->global_mutex);
-	if (forks_assigned)
+	return (is_forks_assigned);
+}
+
+void	eating(t_philo *philo, t_table *table)
+{
+	int	left3;
+	int	left2;
+	int	right2;
+
+	if (forks_taken(philo, table))
 	{
-		pthread_mutex_lock(&(table->fork_mutex[num]));
+		pthread_mutex_lock(&(table->fork_mutex[philo->num]));
 	}
 	philo->status = EAT;
-	log_status(philo, "Eating");
+	log_status(philo, "is eating");
 	philo->eat_clock = get_time_in_ms();
-	usleep(table->eat_time);
-	log_status(philo, "Finished Eating");
+	usleep(table->eat_time * 1000);
 	pthread_mutex_lock(table->global_mutex);
 	philo->status = SLEEP;
 	table->fork_status[philo->left_fork] = FREE;
 	table->fork_status[philo->right_fork] = FREE;
-	left2 = round_left(philo->left_fork, nb_philo);
-	right2 = round_right(philo->right_fork, nb_philo);
+	left2 = round_left(philo->left_fork, philo->table->nb_philo);
+	right2 = round_right(philo->right_fork, philo->table->nb_philo);
 	if (table->fork_status[right2] == FREE
 		&& table->philos[philo->right_fork].status == THINK)
 	{
-		table->fork_status[philo->right_fork] = TAKEN;
-		log_status(&table->philos[philo->right_fork], "Has taken a fork");
-		table->fork_status[right2] = TAKEN;
-		log_status(&table->philos[philo->right_fork], "Has taken a fork");
-		pthread_mutex_unlock(&(table->fork_mutex[philo->right_fork]));
+		if (table->philos[right2].status != THINK
+			|| table->philos[right2].eat_clock >= \
+			table->philos[philo->right_fork].eat_clock)
+		{
+			table->fork_status[philo->right_fork] = TAKEN;
+			log_status(&table->philos[philo->right_fork], "Has taken a fork");
+			table->fork_status[right2] = TAKEN;
+			log_status(&table->philos[philo->right_fork], "Has taken a fork");
+			pthread_mutex_unlock(&(table->fork_mutex[philo->right_fork]));
+		}
 	}
 	if (table->fork_status[left2] == FREE
-		&& table->philos[philo->left_fork].status == THINK)
+		&& table->philos[left2].status == THINK)
 	{
-		table->fork_status[philo->left_fork] = TAKEN;
-		log_status(&table->philos[philo->left_fork], "Has taken a fork");
-		table->fork_status[left2] = TAKEN;
-		log_status(&table->philos[philo->left_fork], "Has taken a fork");
-		pthread_mutex_unlock(&(table->fork_mutex[philo->left_fork]));
+		left3 = round_left(left2, philo->table->nb_philo);
+		if (table->philos[left3].status != THINK
+			|| table->philos[left3].eat_clock >= table->philos[left2].eat_clock)
+		{
+			table->fork_status[philo->left_fork] = TAKEN;
+			log_status(&table->philos[left2], "Has taken a fork");
+			table->fork_status[left2] = TAKEN;
+			log_status(&table->philos[left2], "Has taken a fork");
+			pthread_mutex_unlock(&(table->fork_mutex[left2]));
+		}
 	}
 	pthread_mutex_unlock(table->global_mutex);
 }
 
 void	sleeping(t_philo *philo)
 {
-	usleep(philo->table->sleep_time);
+	log_status(philo, "is sleeping");
+	usleep(philo->table->sleep_time * 1000);
 }
 
 void	*dinner(void *void_philo)
 {
 	t_philo	*philo;
+	t_table	*table;
 
 	philo = void_philo;
 	table = philo->table;
-	while (table->is_all_safe)
+	if (philo->num % 2 == 1)
+		usleep(philo->table->eat_time);
+	while (philo->table->dinner_inprogress)
 	{
 		thinking(philo);
-		eating(philo);
+		eating(philo, table);
 		sleeping(philo);
 	}
 	return (0);
